4ed6c2e9bf2eb17942ca94c056746acaeecfe358
Denis Chmel <denis.chmel@gmail.com>
2014-09-17 10:15:07 +0000

    Part 2

--- wp-content/themes/agency-pro/page_landing2.php
+++ wp-content/themes/agency-pro/page_landing2.php
@@ -11,38 +11,99 @@
 Template Name: Landing2
 */
 ?>
-
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <title><?php wp_title(); ?></title>
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-    </head>
-111
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Bask Business</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="http://portal.bask.local/vendor/bootstrap/dist/css/bootstrap.min.css" type="text/css" media="all"/>
+    <script type="text/javascript" src="http://portal.bask.local/vendor/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="http://portal.bask.local/css/main.css" type="text/css" media="all"/>
+</head>
+<body class="guest_home">
+
+<div class="layout-wrapper">
+    <div class="layout-row">
+        <main class="layout">
+
+            <div class="navbar navbar-inverse" role="navigation">
+
+                <div class="page-progressbar"></div>
+
+                <div class="top-message">
+                </div>
+
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"><img src="http://portal.bask.local/img/bask-business.png"></a>
+                    </div>
+
+                    <div class="collapse navbar-collapse">
+                    </div>
+                </div>
+            </div>
+
+            <div class="page-inner">
+
+
+
 <?php
 
-//if ( have_posts() ) : while ( have_posts() ) : 
 the_post();
-
 do_action( 'genesis_post_content' );
 
-//genesis_legacy_loop();
-return;
-do_action( 'genesis_entry_header' );
 
-	
-			do_action( 'genesis_before_entry_content' );
-			printf( '<div %s>', genesis_attr( 'entry-content' ) );
-				do_action( 'genesis_entry_content' );
-			echo '</div>'; //** end .entry-content
-			do_action( 'genesis_after_entry_content' );
-			
-			do_action( 'genesis_entry_footer' );
-	
+?>
+            </div>
 
-//do_action( 'genesis_loop' );
+        </main>
+    </div>
+    <div class="layout-row">
+
+        <footer class="layout">
+            <div class="container">
+
+                <div class="pull-right text-right">
+
+                    <div class="social-icons">
+                        <a href="https://twitter.com/GoBask"><img src="http://gobask.com/release/img/social/twitter.png" alt="twitter"></a>&nbsp;&nbsp;
+                        <a href="https://www.facebook.com/gobask"><img src="http://gobask.com/release/img/social/facebook.png" alt="facebook"></a>&nbsp;&nbsp;
+                        <a href="http://instagram.com/gobask/"><img src="http://gobask.com/release/img/social/insta.png" alt="instagram"></a>&nbsp;&nbsp;
+                        <a href="http://www.pinterest.com/GoBask/"><img src="http://gobask.com/release/img/social/pinterest.png" alt="pinterest"></a>
+                    </div>
+
+                </div>
+
+                <p class="links">
+                    <a href="#">What's bask</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Jobs</a>
+                    <a href="#">Terms</a>
+                    <a href="#">Privacy</a>
+                </p>
+
+                <div id="logo-gobask">
+                    <h1>Bask</h1>
+
+                    <h2>© 2014 Bask Labs Inc.</h2>
+                </div>
+
+            </div>
+
+        </footer>
+
+    </div>
+</div>
+
+<script type="text/javascript" src="http://portal.bask.local/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
 
